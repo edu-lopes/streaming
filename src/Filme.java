9a6c5c2e0e69ff53935e5e2ddbb47f30931a8fd9
Guilherme de Lima Ficagna <guilhermedelima925@gmail.com>
2026-09-01 22:15:40 -0300

    estende atributos e métodos de Midia para Filme e Serie

--- src/Filme.java
+++ src/Filme.java
@@ -1,44 +1,16 @@
-public class Filme {
-    private String titulo;
-    private String sinopse;
+public class Filme extends Midia {
     private int duracaoMinutos;
-    private String planoNecessario;
-
 
     public Filme(String titulo, String sinopse, int duracaoMinutos, String planoNecessario) {
+        super(titulo, sinopse, planoNecessario);
+
         // Validações
-        if (titulo == null || titulo.isBlank()) {
-            throw new IllegalArgumentException("O título não pode ser vazio.");
-        }
-
-        if (sinopse == null || sinopse.isBlank()) {
-            throw new IllegalArgumentException("A sinopse não pode ser vazia.");
-        }
-
         if (duracaoMinutos <= 0) {
             throw new IllegalArgumentException("A duração do filme deve ser maior que zero.");
         }
 
-        if (planoNecessario == null || planoNecessario.isBlank()) {
-            throw new IllegalArgumentException("O plano necessário não pode ser vazio.");
-        }
-
-        this.titulo = titulo;
-        this.sinopse = sinopse;
         this.duracaoMinutos = duracaoMinutos;
-        this.planoNecessario = planoNecessario;
     }
 
-
-    public void exibirInfo() {
-        System.out.println("Filme: " + titulo + ". Sinopse filme: " + sinopse +
-                ". Duração: " + duracaoMinutos + " min.");
-    }
-    public String getPlanoNecessario() {
-        return planoNecessario;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
+    // fazer a atividade 08 aq utilizando como override o exibirInfo()
 }
--- src/Serie.java
+++ src/Serie.java
@@ -1,28 +1,11 @@
 import java.util.ArrayList;
 
-public class Serie {
-    private String titulo;
-    private String sinopse;
-    private String planoNecessario;
+public class Serie extends Midia{
     private ArrayList<Episodio> episodios;
 
     public Serie(String titulo, String sinopse, String planoNecessario) {
-        // Validações
-        if (titulo == null || titulo.isBlank()) {
-            throw new IllegalArgumentException("O título da série não pode ser vazio.");
-        }
+        super(titulo, sinopse, planoNecessario);
 
-        if (sinopse == null || sinopse.isBlank()) {
-            throw new IllegalArgumentException("A sinopse da série não pode ser vazia.");
-        }
-
-        if (planoNecessario == null || planoNecessario.isBlank()) {
-            throw new IllegalArgumentException("O plano necessário não pode ser vazio.");
-        }
-
-        this.titulo = titulo;
-        this.sinopse = sinopse;
-        this.planoNecessario = planoNecessario;
         this.episodios = new ArrayList<>();
     }
 
@@ -30,19 +13,5 @@ public class Serie {
         episodios.add(episodio);
     }
 
-    public void exibirInfo() {
-        System.out.println("Série: " + titulo + ". Sinopse série: " + sinopse + ".");
-
-        for (Episodio episodio : episodios) {
-            System.out.println(episodio.exibirEpisodio());
-        }
-    }
-
-    public String getPlanoNecessario() {
-        return planoNecessario;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
+    // fazer a atividade 08 aq utilizando como override o exibirInfo()
 }
